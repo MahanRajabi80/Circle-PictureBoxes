a72c4f4d3c3d73a4b76ff9e2f72b622b9bbb798f
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 19:11:15 +0330

    Add files via upload

---
+++ Example 16-4/Example 16-4/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Example_16_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAddPicture = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAddPicture
+            // 
+            this.btnAddPicture.Location = new System.Drawing.Point(231, 12);
+            this.btnAddPicture.Name = "btnAddPicture";
+            this.btnAddPicture.Size = new System.Drawing.Size(102, 37);
+            this.btnAddPicture.TabIndex = 0;
+            this.btnAddPicture.Text = "Add Picture";
+            this.btnAddPicture.UseVisualStyleBackColor = true;
+            this.btnAddPicture.Click += new System.EventHandler(this.btnAddPicture_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 466);
+            this.Controls.Add(this.btnAddPicture);
+            this.Name = "Form1";
+            this.Text = "Example 16-4 (Circle PictureBoxes)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddPicture;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Example 16-4/Example 16-4/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_16_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------------------------
+        System.Collections.ArrayList pictureList = new System.Collections.ArrayList();
+        //-----------------------------------------------------------------------------
+        private void btnAddPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Jpeg Files (*.jpg)|*.jpg|Bitmap Files (*.bmp)|*.bmp";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.SafeFileName != "")
+            {
+                PictureBox pic = new PictureBox();
+                pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                pic.Size = new System.Drawing.Size(60, 60);
+                pic.Image = Image.FromFile(openFileDialog1.FileName);
+                pictureList.Add(pic);
+                this.Controls.Add(pic);
+            }
+        }
+        //-----------------------------------------------------------------------------
+        int teta = 90;
+        const int radius = 100;
+        //-----------------------------------------------------------------------------
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureList.Count == 0)
+                return;
+            //----------------------------//
+            int x = this.ClientSize.Width / 2; //center x
+            int y = this.ClientSize.Height / 2; //center y
+            for (int i = 0,t=teta; i < pictureList.Count; i++)
+            {
+                int dx = (int)(radius * Math.Cos(Radian(t)));
+                int dy = (int)(radius * Math.Sin(Radian(t)));
+                ((PictureBox)pictureList[i]).Location = new Point(x+dx,y-dy);
+                t=(t+360/pictureList.Count)%360;//donbale aghab
+            }
+            teta--;
+            if (teta == -1)
+                teta = 359;
+        }
+        //-----------------------------------------------------------------------------
+        private static double Radian(double d)
+        {
+            return d * Math.PI / 180;
+        }
+    }
+}
